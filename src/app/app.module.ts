46946f3edce61523dc5972edf54fc23d15337a14
Gledson Carvalho <gledsoncarvalho99@hotmail.com>
2018-01-22 17:30:13 -0200

    Home.ts compartilhada?

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,19 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { IonicStorageModule } from "@ionic/storage";
+import { HttpModule } from '@angular/http';
+
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { DatePipe } from "@angular/common";
+import { UserProvider } from "../providers/user/user";
+import { Geolocation } from '@ionic-native/geolocation';
+import { AngularFireModule } from "angularfire2";
+import { AngularFireDatabaseModule } from "angularfire2/database";
+import { FirebaseProvider } from '../providers/firebase/firebase';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +24,18 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicStorageModule.forRoot(),
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyAVOnXFPxLL7c9Xi0oUV7-8UxiTyxck2bM",
+      authDomain: "sirius-b77ba.firebaseapp.com",
+      databaseURL: "https://sirius-b77ba.firebaseio.com",
+      projectId: "sirius-b77ba",
+      storageBucket: "sirius-b77ba.appspot.com",
+      messagingSenderId: "54967610528"
+    }),
+    AngularFireDatabaseModule,
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,7 +45,13 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    IonicStorageModule,
+    DatePipe,
+    UserProvider,
+    FirebaseProvider,
+    Geolocation,
+    FirebaseProvider
   ]
 })
 export class AppModule {}
